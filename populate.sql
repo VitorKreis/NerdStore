4a07d8d81fab4ff438338357e9199112dfedd63b
VitorKreis <101025640+VitorKreis@users.noreply.github.com>
2024-02-05 10:22:29 -0300

    feat: :sparkles: Criando a tabela artistas e mangas | Inserindo valores iniciais

--- populate.sql
+++ populate.sql
@@ -8,12 +8,6 @@ CREATE TABLE autores (
 );
 
 
-INSERT INTO autores (nome, nacionalidade, idade, createdAt, updatedAt) 
-VALUES  ("JRR Tolkien", "sul-africano", 81, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-        ("Ursula LeGuin", "estadunidense", 88, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-        ("Machado de Assis", "brasileira", 69, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-
 CREATE TABLE livros(
   id            INTEGER NOT NULL PRIMARY KEY,
   titulo          TEXT    NOT NULL,
@@ -26,8 +20,38 @@ CREATE TABLE livros(
 );
 
 
+CREATE TABLE artistas(
+  id            INTEGER NOT NULL PRIMARY KEY,
+  nome          TEXT    NOT NULL,
+  nacionalidade TEXT    NOT NULL,
+  idade         INTEGER NOT NULL,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-<<<<<<< HEAD
+
+CREATE TABLE mangas(
+  id            INTEGER NOT NULL PRIMARY KEY,
+  titulo          TEXT    NOT NULL,
+  capitulo       INTEGER NOT NULL,
+  sinopse          TEXT    NOT NULL,
+  autor_id      INTEGER NOT NULL,
+  artista_id    INTEGER NOT NULL,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (autor_id) REFERENCES autores (id),
+  FOREIGN KEY (artista_id) REFERENCES artistas(id)
+);
+
+
+
+INSERT INTO autores (nome, nacionalidade, idade) 
+VALUES  ("JRR Tolkien", "sul-africano", 81),
+        ("Ursula LeGuin", "estadunidense", 88),
+        ("Machado de Assis", "brasileira", 69,);
+
+
+
 INSERT INTO livros(titulo, paginas, autor_id, sinopse)
 VALUES ('Hobbit', 230, 1, 
 'Bilbo Bolseiro é um hobbit que leva uma vida confortável e sem ambições. Mas seu contentamento é perturbado quando Gandalf, o mago, e uma companhia de anões batem à sua porta e levam-no para uma expedição'),
@@ -35,9 +59,18 @@ VALUES ('Hobbit', 230, 1,
 ('A mão esquerda da escuridão', 304, 2, 'Enviado em uma missão intergaláctica, Genly Ai, um humano, tem como missão persuadir os governantes do planeta Gethen a se unirem a uma comunidade universal. Entretanto, Genly, mesmo depois de anos de estudo, percebe-se despreparado para a situação que lhe aguardava. Ao entrar em contato com uma cultura complexa, rica, quase medieval e com outra abordagem na relação entre os gêneros, Genly perde o controle da situação.');
 
 
-=======
-INSERT INTO livros(titulo, paginas, autor_id)
-VALUES ('Hobbit', 230, 1),
-('Senhor do Aneis | A Socidade do Anel', 400, 1);
->>>>>>> main
+INSERT INTO autores(nome, nacionalidade, idade)
+VALUES ('Yuki Tabata', 'japones', 39),
+        ('Eiichiro Oda', 'japones', 49),
+        ('Yutaka Nanten', 'japones' , 40);
+
+
+INSERT INTO mangas(titulo, capitulo, sinopse, autor_id, artista_id)
+VALUES ('Black Clover', 4, 'A história acompanha os dois garotos que competem entre si para se tornar o Rei Mago, o cavaleiro mágico mais forte do reino de Clover. Mesmo sem magia, Asta tenta ser um cavaleiro mágico, assim sua jornada começa quando obtém o misterioso poder "antimagia", que pode anular qualquer magia na obra',
+        4, 1),
+        ('One Piece', 120, 'A trama segue Monkey D. Luffy, um jovem que tem a ambição de se tornar o Rei dos Piratas - aquele que consegue todas as coisas do mundo. Para isso, ele precisa encontrar o One Piece, o tesouro lendário deixado por Gol D. Roger, primeiro rei da pirataria',
+        5, 2),
+        ('Cowboy Bebop', 8, 'Em Cowboy Bebop, estamos no ano de 2071. A humanidade se espalhou pelos planetas do Sistema Solar graças aos “gates”, vias expressas espaciais que interligam o Sistema Solar. Abusando da tecnologia, os criminosos também ficaram bem mais perigosos',
+        6, 3);
+
 
