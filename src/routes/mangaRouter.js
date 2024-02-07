763dd09291d3e55c1b453a6ff726a2b250d166e5
VitorKreis <101025640+VitorKreis@users.noreply.github.com>
2024-02-07 14:46:25 -0300

    feat: :sparkles: Add pegarPorID com join de autor e artista id

--- src/routes/mangaRouter.js
+++ src/routes/mangaRouter.js
@@ -4,6 +4,7 @@ import MangaController from '../controller/MangaController.js';
 
 const router = Router();
 
-router.get('/Mangas', MangaController.pegarTodos);
+router.get('/mangas', MangaController.pegarTodos);
+router.get('/manga/:id', MangaController.pegarPorID);
 
 export default router;
--- src/service/MangaService.js
+++ src/service/MangaService.js
@@ -1,8 +1,26 @@
-import { Manga } from '../models/index.js';
+import { Artista, Autor, Manga } from '../models/index.js';
 
 class MangaService {
   async pegarTodos() {
     const resultado = await Manga.findAll();
+
+    return resultado;
+  }
+
+  async pegarPorID(id) {
+    const resultado = await Manga.findByPk(id, {
+      include: [{
+        model: Autor,
+        required: true,
+        attributes: ['id', 'nome'],
+      },
+      {
+        model: Artista,
+        required: true,
+        attributes: ['id', 'nome'],
+      },
+      ],
+    });
 
     return resultado;
   }
